a1e8d35b0edc422f1d8e93359745e5407e70d9af
Muembia <jonathanmuembia3@gmail.com>
2026-05-16 22:11:04 +0200

    validator added for contact fields

---
+++ middleware/validator.js
@@ -0,0 +1,29 @@
+const { body, validationResult } = require('express-validator');
+
+
+const contactValidationRules = () => {
+  return [
+    body('firstName').trim().notEmpty().withMessage('firstName is required.'),
+    body('lastName').trim().notEmpty().withMessage('lastName is required.'),
+    body('email').isEmail().withMessage('Valide Emaill is required.').normalizeEmail(),
+    body('favoriteColor').trim().notEmpty().withMessage('favoriteColor is required'),
+    body('birthday').isISO8601().withMessage('birthday should be in this format (MMM-DD-YYY)')
+  ];
+};
+
+const checkValidationResult = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next(); 
+  }
+  
+  return res.status(422).json({
+    success: false,
+    errors: errors.array().map(err => ({ field: err.path, message: err.msg }))
+  });
+};
+
+module.exports = {
+  contactValidationRules,
+  checkValidationResult
+};
--- routes/contact.js
+++ routes/contact.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const contactController = require('../controllers/contactController');
+const { contactValidationRules, checkValidationResult } = require('../middleware/validator');
 
 router.get('/', contactController.homePage);
 router.get('/contact', contactController.getAllContacts);
 router.get('/contact/:id', contactController.getContactById);
-router.post('/contact/add', contactController.addContact);
+router.post('/contact/add',contactValidationRules(), checkValidationResult,  contactController.addContact);
 router.delete('/contact/delete/:id', contactController.deleteContact);
-router.put('/contact/update/:id', contactController.updateContact);
+router.put('/contact/update/:id',contactValidationRules(), checkValidationResult, contactController.updateContact);
 
 
 module.exports = router;
